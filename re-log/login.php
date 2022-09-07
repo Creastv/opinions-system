be2929af184c4c0d19a36469d1f1da773c8810b2
Creastv <piotr@roial.pl>
2022-09-07 15:58:57 +0200

    reCatcha reperig

--- re-log/login.php
+++ re-log/login.php
@@ -38,51 +38,51 @@
                    <p class="text-center">Nie posiadasz jeszcze konta? <a href="<?php echo esc_url( home_url( '/rejestracja/' ) ); ?>">  Zarejestruj się</a></p>
                 </div>
         </form>
-<?php } else {
-        echo '<p>Jesteś już zalogowany</p>';
-        // wp_redirect(site_url('/panel-uzytkownika/'));
+    <?php } else {
         $url = site_url('/panel-uzytkownika/');
         echo("<script>location.href = '".$url."'</script>");
     }
+
     echo '</div>';
     echo '</div>';
       $login_form = ob_get_clean();
       return $login_form;
   }
 
-    $recaptcha = $_POST['g-recaptcha-response'];
-    $res = reCaptcha($recaptcha);
-    if($res['success']){
-    add_action('wp', 'wc_user_login_callback');
-    }else{
-    // Error
-    }
-    function reCaptcha($recaptcha){
-    $secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
-    //  $secret = "6Le_WLwhAAAAAI-wLBRU7yYMb-CF45lVlihUb9Ra";
-    $ip = $_SERVER['REMOTE_ADDR'];
-
-    $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
-    $url = "https://www.google.com/recaptcha/api/siteverify";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-    $data = curl_exec($ch);
-    curl_close($ch);
-
-    return json_decode($data, true);
-    }
+   
 
   function wc_user_login_callback() {
       if (isset($_POST['formType']) && wp_verify_nonce($_POST['formType'], 'userLogin')) {
 
           global $errors_login;
+
           $uName = $_POST['log'];
           $uPassword = $_POST['pwd'];
           $redirect = $_POST['redirect'];
 
+          $recaptcha = $_POST['g-recaptcha-response'];
+          $res = reCaptcha($recaptcha);
+
+        function reCaptcha($recaptcha){
+            $secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
+            //  $secret = "6Le_WLwhAAAAAI-wLBRU7yYMb-CF45lVlihUb9Ra";
+            $ip = $_SERVER['REMOTE_ADDR'];
+
+            $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
+            $url = "https://www.google.com/recaptcha/api/siteverify";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+            return json_decode($data, true);
+        }
+      if (!$res['success']) {
+              $errors_login = '<strong>Error! </strong> Nazwa urzytkownika jest wymagana.';
+      }
           if ($uName == '' && $uPassword != '') {
               $errors_login = '<strong>Error! </strong> Nazwa urzytkownika jest wymagana.';
           } elseif ($uName != '' && $uPassword == '') {
@@ -99,7 +99,7 @@
                   $errors_login = $user->get_error_message();
               } else {
                     wp_redirect(site_url('/panel-uzytkownika/'));
-                  exit;
+                exit;
               }
           }
       }
