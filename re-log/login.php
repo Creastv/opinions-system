0136961699721618d055a6ba5d360cb20b27f2b6
Creastv <piotr@roial.pl>
2022-09-07 16:20:50 +0200

    reCatcha reperig

--- re-log/login.php
+++ re-log/login.php
@@ -23,7 +23,7 @@
                 </div>
                 <div class="form-group col col-1">
                     <!-- <div class="g-recaptcha brochure__form__captcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div> -->
-                    <div class="g-recaptcha brochure__form__captcha" data-sitekey="6Le_WLwhAAAAAHilEH4trnb6OTffXBjb68BOeVtm"></div>
+                    <!-- <div class="g-recaptcha brochure__form__captcha" data-sitekey="6Le_WLwhAAAAAHilEH4trnb6OTffXBjb68BOeVtm"></div> -->
                 </div>
                 <?php
                         ob_start();
@@ -39,74 +39,20 @@
                 </div>
         </form>
     <?php } else {
+        echo '<p>Jesteś już zalogowany</p>';
+        // wp_redirect(site_url('/panel-uzytkownika/'));
         $url = site_url('/panel-uzytkownika/');
         echo("<script>location.href = '".$url."'</script>");
     }
-    
     echo '</div>';
     echo '</div>';
-      $login_form = ob_get_clean();
-      return $login_form;
-  }
 
-    // $recaptcha = $_POST['g-recaptcha-response'];
-    // $res = reCaptcha($recaptcha);
-    // if($res['success']){
-    // add_action('wp', 'wc_user_login_callback');
-    // }else{
-    //  $errors_login = '<strong>Error! </strong> Nazwa urzytkownika i hasło są wymagane';
-    // }
-    // function reCaptcha($recaptcha){
-    // // $secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
-    // $secret = "6Le_WLwhAAAAAI-wLBRU7yYMb-CF45lVlihUb9Ra";
-    // $ip = $_SERVER['REMOTE_ADDR'];
-
-    // $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
-    // $url = "https://www.google.com/recaptcha/api/siteverify";
-    // $ch = curl_init();
-    // curl_setopt($ch, CURLOPT_URL, $url);
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-    // $data = curl_exec($ch);
-    // curl_close($ch);
-
-    // return json_decode($data, true);
-    // }
-
-  function wc_user_login_callback() {
-      if (isset($_POST['formType']) && wp_verify_nonce($_POST['formType'], 'userLogin')) {
-        
+    if (isset($_POST['formType']) && wp_verify_nonce($_POST['formType'], 'userLogin')) {
 
           global $errors_login;
           $uName = $_POST['log'];
           $uPassword = $_POST['pwd'];
           $redirect = $_POST['redirect'];
-
-
-          $recaptcha = $_POST['g-recaptcha-response'];
-            $res = reCaptcha($recaptcha);
-            if(!$res['success']){
-            
-            $errors_login = '<strong>Error! </strong> Nazwa urzytkownika i hasło są wymagane';
-            }
-            function reCaptcha($recaptcha){
-            // $secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
-            $secret = "6Le_WLwhAAAAAI-wLBRU7yYMb-CF45lVlihUb9Ra";
-            $ip = $_SERVER['REMOTE_ADDR'];
-
-            $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
-            $url = "https://www.google.com/recaptcha/api/siteverify";
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-            $data = curl_exec($ch);
-            curl_close($ch);
-
-            return json_decode($data, true);
-            }
 
           if ($uName == '' && $uPassword != '') {
               $errors_login = '<strong>Error! </strong> Nazwa urzytkownika jest wymagana.';
@@ -128,4 +74,81 @@
               }
           }
       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      $login_form = ob_get_clean();
+      return $login_form;
   }
+
+    // $recaptcha = $_POST['g-recaptcha-response'];
+    // $res = reCaptcha($recaptcha);
+    // if($res['success']){
+    // add_action('wp', 'wc_user_login_callback');
+    // }else{
+    // // Error
+    // }
+    // function reCaptcha($recaptcha){
+    // // $secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
+    //  $secret = "6Le_WLwhAAAAAI-wLBRU7yYMb-CF45lVlihUb9Ra";
+    // $ip = $_SERVER['REMOTE_ADDR'];
+
+    // $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
+    // $url = "https://www.google.com/recaptcha/api/siteverify";
+    // $ch = curl_init();
+    // curl_setopt($ch, CURLOPT_URL, $url);
+    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    // curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+    // $data = curl_exec($ch);
+    // curl_close($ch);
+
+    // return json_decode($data, true);
+    // }
+
+//   function wc_user_login_callback() {
+//       if (isset($_POST['formType']) && wp_verify_nonce($_POST['formType'], 'userLogin')) {
+
+//           global $errors_login;
+//           $uName = $_POST['log'];
+//           $uPassword = $_POST['pwd'];
+//           $redirect = $_POST['redirect'];
+
+//           if ($uName == '' && $uPassword != '') {
+//               $errors_login = '<strong>Error! </strong> Nazwa urzytkownika jest wymagana.';
+//           } elseif ($uName != '' && $uPassword == '') {
+//               $errors_login = '<strong>Error! </strong> Hało jest wymagane.';
+//           } elseif ($uName == '' && $uPassword == '') {
+//               $errors_login = '<strong>Error! </strong> Nazwa urzytkownika i hasło są wymagane.';
+//           } elseif ($uName != '' && $uPassword != '') {
+//               $creds = array();
+//               $creds['user_login'] = $uName;
+//               $creds['user_password'] = $uPassword;
+//               $creds['remember'] = false;
+//               $user = wp_signon($creds, true);
+//               if (is_wp_error($user)) {
+//                   $errors_login = $user->get_error_message();
+//               } else {
+//                     wp_redirect(site_url('/panel-uzytkownika/'));
+//                   exit;
+//               }
+//           }
+//       }
+//   }
