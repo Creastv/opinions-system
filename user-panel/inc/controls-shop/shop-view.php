e3cdea55b0b319bcd04c20a360332f22b700fadc
Creastv <piotr@roial.pl>
2022-09-07 15:35:43 +0200

    checking and fixing panel

--- user-panel/inc/controls-shop/shop-view.php
+++ user-panel/inc/controls-shop/shop-view.php
@@ -1,6 +1,9 @@
 <?php 
 function o_system_view_shop(){
-  return '<a href=" ' . get_permalink( $current_user->id ) . ' " target="_blank" class="o-systm-btn">Idź do wizytówki sklepu</a> ';
+  ob_start();
+  echo '<a href=" ' . get_permalink( $current_user->id ) . ' " target="_blank" class="o-systm-btn">Idź do wizytówki sklepu</a> ';
+  $view_shop = ob_get_clean();
+  return $view_shop;
 }
 
 add_action('wp', 'o_system_view_shop');
