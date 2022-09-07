c321616cd46db2462fc3211f57ee803ce7ba592b
Creastv <piotr@roial.pl>
2022-09-07 16:10:20 +0200

    reCatcha reperig

--- re-log/login.php
+++ re-log/login.php
@@ -49,30 +49,39 @@
       return $login_form;
   }
 
-    // $recaptcha = $_POST['g-recaptcha-response'];
-    // $res = reCaptcha($recaptcha);
-    // if($res['success']){
-    // add_action('wp', 'wc_user_login_callback');
-    // }else{
-    //  $errors_login = '<strong>Error! </strong> Nie jestem robotem';
-    // }
-
+    
 
   function wc_user_login_callback() {
+    $recaptcha = $_POST['g-recaptcha-response'];
+    $res = reCaptcha($recaptcha);
+    if($res['success']){
+    add_action('wp', 'wc_user_login_callback');
+    }else{
+     $errors_login = '<strong>Error! </strong> Nazwa urzytkownika i hasło są wymagane';
+    }
+    function reCaptcha($recaptcha){
+    // $secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
+    $secret = "6Le_WLwhAAAAAI-wLBRU7yYMb-CF45lVlihUb9Ra";
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($data, true);
+    }
       if (isset($_POST['formType']) && wp_verify_nonce($_POST['formType'], 'userLogin')) {
 
           global $errors_login;
           $uName = $_POST['log'];
           $uPassword = $_POST['pwd'];
           $redirect = $_POST['redirect'];
-
-            $recaptcha = $_POST['g-recaptcha-response'];
-            $res = reCaptcha($recaptcha);
-            if(!$res['success']){
-           
-            $errors_login = '<strong>Error! </strong> Nie jestem robotem';
-            }
-
 
           if ($uName == '' && $uPassword != '') {
               $errors_login = '<strong>Error! </strong> Nazwa urzytkownika jest wymagana.';
@@ -93,29 +102,5 @@
                   exit;
               }
           }
-
-
-
-
-
-
       }
-
-          function reCaptcha($recaptcha){
-    // $secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
-     $secret = "6Le_WLwhAAAAAI-wLBRU7yYMb-CF45lVlihUb9Ra";
-    $ip = $_SERVER['REMOTE_ADDR'];
-
-    $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
-    $url = "https://www.google.com/recaptcha/api/siteverify";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-    $data = curl_exec($ch);
-    curl_close($ch);
-
-    return json_decode($data, true);
-    }
   }
